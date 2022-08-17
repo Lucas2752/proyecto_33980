9face07f9f9ba33413281784d0eade6efd7f5067
lucasCarrera <lucascarrera2752@gmail.com>
2022-08-17 18:53:42 -0300

    agregue js para carrito y style css

---
+++ js/stock.js
@@ -0,0 +1,29 @@
+let stockProductos = [
+    {id: 1, nombre: "Montana 94", tipo: "Aerosol", desc: "Color rojo", precio: 1500, tamaño: "Grande", img: '../img/Montana94.jpg'},
+    {id: 2, nombre: "Montana 94", tipo: "Aerosol", desc: "Color negro", precio: 1500, tamaño: "Grande", img: '../img/Montana94.jpg'},
+    {id: 3, nombre: "Montana 94", tipo: "Aerosol", desc: "Color azul", precio: 1500, tamaño: "Grande", img: '../img/Montana94.jpg'},
+    {id: 4, nombre: "Montana 94", tipo: "Aerosol", desc: "Color blanco", precio: 1500, tamaño: "Grande", img: '../img/Montana94.jpg'},
+    {id: 5, nombre: "Montana 94", tipo: "Aerosol", desc: "Color amarillo", precio: 1500, tamaño: "Grande", img: '../img/Montana94.jpg'},
+    {id: 6, nombre: "Montana 94", tipo: "Aerosol", desc: "Color gris", precio: 1500, tamaño: "Grande", img: '../img/Montana94.jpg'},
+    {id: 7, nombre: "Montana Hardcore", tipo: "Aerosol", desc: "Color rojo", precio: 1300, tamaño: "Chico", img: '../img/MontanaHardcore.jpg'},
+    {id: 8, nombre: "Montana Hardcore", tipo: "Aerosol", desc: "Color negro", precio: 1300, tamaño: "Chico", img: '../img/MontanaHardcore.jpg'},
+    {id: 9, nombre: "Montana Hardcore", tipo: "Aerosol", desc: "Color azul", precio: 1300, tamaño: "Chico", img: '../img/MontanaHardcore.jpg'},
+    {id: 10, nombre: "Montana Hardcore", tipo: "Aerosol", desc: "Color blanco", precio: 1300, tamaño: "Chico", img: '../img/MontanaHardcore.jpg'},
+    {id: 11, nombre: "Montana Hardcore", tipo: "Aerosol", desc: "Color amarillo", precio: 1300, tamaño: "Chico", img: '../img/MontanaHardcore.jpg'},
+    {id: 12, nombre: "Montana Hardcore", tipo: "Aerosol", desc: "Color gris", precio: 1300, tamaño: "Chico", img: '../img/MontanaHardcore.jpg'},
+    {id: 13, nombre: "Montana 94", tipo: "Aerosol", desc: "Color rojo", precio: 998, tamaño: "Chico", img: '../img/Montana94C.jpg'},
+    {id: 14, nombre: "Montana 94", tipo: "Aerosol", desc: "Color negro", precio: 998, tamaño: "Chico", img: '../img/Montana94C.jpg'},
+    {id: 15, nombre: "Montana 94", tipo: "Aerosol", desc: "Color azul", precio: 998, tamaño: "Chico", img: '../img/Montana94C.jpg'},
+    {id: 16, nombre: "Montana 94", tipo: "Aerosol", desc: "Color blanco", precio: 998, tamaño: "Chico", img: '../img/Montana94C.jpg'},
+    {id: 17, nombre: "Montana 94", tipo: "Aerosol", desc: "Color amarillo", precio: 998, tamaño: "Chico", img: '../img/Montana94C.jpg'},
+    {id: 18, nombre: "Montana 94", tipo: "Aerosol", desc: "Color gris", precio: 998, tamaño: "Chico", img: '../img/Montana94C.jpg'},
+    {id: 19, nombre: "Montana Hardcore", tipo: "Aerosol", desc: "Color rojo", precio: 1890, tamaño: "Grande", img: '../img/MontanaHardcoreC.jpg'},
+    {id: 20, nombre: "Montana Hardcore", tipo: "Aerosol", desc: "Color negro", precio: 1890, tamaño: "Grande", img: '../img/MontanaHardcoreC.jpg'},
+    {id: 21, nombre: "Montana Hardcore", tipo: "Aerosol", desc: "Color azul", precio: 1890, tamaño: "Grande", img: '../img/MontanaHardcoreC.jpg'},
+    {id: 22, nombre: "Montana Hardcore", tipo: "Aerosol", desc: "Color blanco", precio: 1890, tamaño: "Grande", img: '../img/MontanaHardcoreC.jpg'},
+    {id: 23, nombre: "Montana Hardcore", tipo: "Aerosol", desc: "Color amarillo", precio: 1890, tamaño: "Grande", img: '../img/MontanaHardcoreC.jpg'},
+    {id: 24, nombre: "Montana Hardcore", tipo: "Aerosol", desc: "Color gris", precio: 1890, tamaño: "Grande", img: '../img/MontanaHardcoreC.jpg'},
+]
+
+
+
